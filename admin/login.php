65f40d8fe6c747e2a75594f2fe17445b03a8d70f
Tristan Kennedy <tristan@taskinoz.com>
2022-05-22 15:31:46 +1000

    Fixed path issues

--- admin/login.php
+++ admin/login.php
@@ -11,7 +11,7 @@
   <title>Login</title>
   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="./admin/login.css">
+  <link rel="stylesheet" href="./login.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
@@ -20,10 +20,10 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <form method="POST" action="./index.php" target="updated-list">
+        <form method="POST" action="./" target="updated-list">
           <div class="form-group">
               <div class="ns-icon">
-                <img src="./admin/img/logo_1k.png" alt="">
+                <img src="./img/logo_1k.png" alt="">
               </div>
             <?php if ($pass!=null) { ?>
               <input type="password" class="form-control error" id="Password" name="pass" placeholder="Incorrect Password">
