283c9558088194108c075ddee9252c8795eecd74
Tristan Kennedy <tristan@taskinoz.com>
2022-05-22 17:22:50 +1000

    Moved login to session to avoid relogin

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,12 @@
 <?php
 // Set the password variable
+session_start();
 if (isset($_POST['pass'])){
   $pass = $_POST['pass'];
+  $_SESSION["pass"] = $pass;
+}
+elseif (isset($_SESSION["pass"])) {
+  $pass = $_SESSION["pass"];
 }
 else {
   $pass = null;
@@ -10,7 +15,7 @@ else {
 // Import Password
 include("password.php");
 
-// If login works show wedding site
+// If login works show site
 if ($pass == $password)
 {
   include("dashboard.php");
